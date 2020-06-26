553ac0a8e5ce78d504fe64251f0fb020c7fac87d
Himanshu Yadav <himanshu24yadav@gmail.com>
2020-06-26 21:03:25 +0530

    First Commit

---
+++ library/src/main/java/com/example/library/CalleryView.kt
@@ -0,0 +1,8 @@
+package com.example.library
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class CalleryView(context: Context?, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
+}
