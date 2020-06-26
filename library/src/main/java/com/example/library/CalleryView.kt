1db8482b403e60d760e80810837acb54bcfcc3aa
Himanshu Yadav <himanshu24yadav@gmail.com>
2020-06-26 21:51:02 +0530

    CalleryView Implementation

--- library/src/main/java/com/example/library/CalleryView.kt
+++ library/src/main/java/com/example/library/CalleryView.kt
@@ -4,5 +4,6 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class CalleryView(context: Context?, attrs: AttributeSet?) : ConstraintLayout(context, attrs) {
+class CalleryView @JvmOverloads constructor(context: Context?, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : CalleryViewImpl(context, attrs, defStyleAttr) {
+
 }
---
+++ library/src/main/java/com/example/library/CalleryViewImpl.kt
@@ -0,0 +1,8 @@
+package com.example.library
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+open class CalleryViewImpl(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : ConstraintLayout(context, attrs, defStyleAttr) {
+}
